daf6825d44c281059c570fbcb3af1a9ff69cd281
hoangbkcntt98 <20166135@student.hust.edu.vn>
2021-03-25 09:04:44 +0700

    merge_conflix

---
+++ algorithms/Pert.js
@@ -0,0 +1,104 @@
+const cdf = require('./Utils.js')
+var gaussian = require('gaussian');
+class Pert {
+    constructor(tasks){
+        this.tasks = tasks
+    }
+    estimateDuration(opt,mos,pes){
+        return (opt+4*mos + pes)/6
+    }
+    standardDeviation(opt,pes){
+        return (pes+opt)/6
+    }
+    getEsCompareValue(par){
+        return par.taskInformation.duration + par.taskInformation.es
+    }
+    getLfCompareValue(suc){
+        return suc.taskInformation.lf-suc.taskInformation.duration
+    }
+    parents(task){
+        return this.tasks.filter(t => (task.parentList.includes(t.taskId)))
+    }
+    childs(task){
+        return this.tasks.filter(t => (t.parentList.includes(task.taskId)))
+    }
+    updateChildParent(){
+        console.log('update task')
+        for(let t of this.tasks){
+            t.parents = this.parents(t)
+            t.childs = this.childs(t)
+        }
+    }
+    getSlack(task){
+        return t.taskInformation.ls -t.taskInformation.es
+    }
+    getParents(task){
+        console.log(task.parents)
+    }
+    updateTaskList(){
+        firstTask = this.tasks[0]
+        firstTask.taskInformation.es = 0;
+        for(let t of this.tasks){
+            if(t.parents.length ==0){
+                t.taskInformation.es = 0;
+            }else{
+                let initEs = 0
+                for(let par of t.parents){
+                    let compare = this.getEsCompareValue(par)
+                    if(initEs < compare){
+                        initEs = compare
+                    }
+                }
+                t.taskInformation.es = initEs
+               
+            }
+            t.taskInformation.ef = this.getEsCompareValue(par)
+        }
+
+        let endTask = this.tasks[this.tasks.length-1]
+        endTask.taskInformation.lf = this.getEsCompareValue(endTask)
+        let reverseTasks = this.tasks.reverse()
+        for(let t of reverseTasks){
+            if(t.childs.length==0){
+                t.taskInformation.lf = this.getEsCompareValue(t)
+            }else{
+                let initLf = 100000
+                for(let child of t.childs){
+                    if(child.taskInformation.ls<initLf){
+                        initLf = child.taskInformation.ls
+                    }
+                }
+                t.taskInformation.lf = initLf
+            }
+            t.taskInformation.ls = this.getLfCompareValue(t)
+            t.taskInformation.slack = this.getSlack(t)
+        }
+
+    }
+
+    updatePertProb(){
+        for(let t of this.tasks){
+            let duration = t.taskInformation.duration
+            let opt = t.taskInformation.optimistic
+            let mos = t.taskInformation.mostlikely
+            let pes = t.taskInformation.pessimistic
+            let expectedTime = this.estimateDuration(opt,mos,pes)
+            let standardDeviation = this.standardDeviation(opt,pes)
+            // let prob = cdf(expectedTime,standardDeviation,duration)
+            var distribution = gaussian(expectedTime, standardDeviation*standardDeviation);
+            let prob = distribution.cdf(duration)
+            t.pertProb = prob
+        }
+    }
+    getCriticalPath(){
+        console.log(this.tasks.filter(t => t.taskInformation.slack==0).map(t1=> t1.taskId))
+        return this.tasks.filter(t => t.taskInformation.slack==0)
+    }
+}
+console.log('start PERT test')
+const taskList = require('./test/TaskDataSample')
+let pert = new Pert(taskList)
+pert.updateChildParent()
+pert.updatePertProb()
+pert.getCriticalPath()
+// console.log(taskList)
--- algorithms/test/Test.js
+++ algorithms/test/Test.js
@@ -1,11 +1,18 @@
 import RiskNet from '../impl/RiskNet.js'
 import riskList from './DataSample.js'
+import taskList from './TaskDataSample.js'
 import Array2d from 'array-2d'
+import Pert from '../Pert.js'
 // Test Risk Net
-let riskNet = new RiskNet('riskNet',riskList)
-riskNet.init()
-riskNet.updateProb()
-let risks = riskNet.getRiskList()
-risks.forEach(element => {
-    console.log(element.prob)
-});
+// let riskNet = new RiskNet('riskNet',riskList)
+// riskNet.init()
+// riskNet.updateProb()
+// let risks = riskNet.getRiskList()
+// risks.forEach(element => {
+//     console.log(element.prob)
+// });
+// Test Pert
+// console.log('start PERT test')
+// let pert = new Pert(taskList)
+// pert.updateChildParent
+// console.log(taskList)
